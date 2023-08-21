1c427bf89d046ed13ceb4081bd58e630df37494a
Prakhar <agrawalprakhar893@gmail.com>
2023-08-21 13:29:22 +0530

    Week -3

---
+++ Exercise-2/script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Prompt the user for their name
+var userName = prompt("Please enter your name:");
+
+// Display a greeting in an alert box
+if (userName) {
+  var greeting = "Hello, " + userName + "! Welcome!";
+  alert(greeting);
+} else {
+  alert("Hello there! Welcome!");
+}
